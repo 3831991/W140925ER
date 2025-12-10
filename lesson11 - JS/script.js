6683005d3ba4c5442f8df3d352aab8fe2571a937
Your Name <you@example.com>
2025-12-10 20:40:24 +0200

    added

---
+++ lesson11 - JS/script.js
@@ -0,0 +1,13 @@
+function welcome() {
+    alert("ברוכים הבאים לאתר שלי!");
+}
+
+function welcome2() {
+    let un = document.getElementById("username").value;
+
+    alert("ברוך הבא " + un + "!!!");
+}
+
+function multi50() {
+    
+}
