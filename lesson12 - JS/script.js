21bdbc2360f6500ee3724a6ee93faa58ec0d3e6e
Your Name <you@example.com>
2025-12-24 19:50:36 +0200

    added

--- lesson12 - JS/script.js
+++ lesson12 - JS/script.js
@@ -9,3 +9,13 @@ function task2() {
     document.getElementById("counter2").innerHTML = ++counter2;
     document.getElementById("counter2").style.fontSize = (12 + counter2) + 'px';
 }
+
+function task3() {
+    const answer = document.getElementById("answer").value;
+
+    if (answer == "צהוב") {
+        document.getElementById("output").innerHTML = "נכון 🍌";
+    } else {
+        document.getElementById("output").innerHTML = "לא נכון";
+    }
+}
