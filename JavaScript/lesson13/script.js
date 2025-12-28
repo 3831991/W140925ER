b2c3746064d0a1143171a05a7938330deaf5bb72
Your Name <you@example.com>
2025-12-28 19:07:54 +0200

    added

--- JavaScript/lesson13/script.js
+++ JavaScript/lesson13/script.js
@@ -1,3 +1,13 @@
 function task1() {
-    
+    const task = document.querySelector("#task1");
+    const n = +task.querySelector("input").value;
+    const output = task.querySelector("span");
+
+    if (n % 2) {
+        output.innerText = "אי זוגי";
+        output.style.color = "red";
+    } else {
+        output.innerText = "זוגי";
+        output.style.color = "blue";
+    }
 }
