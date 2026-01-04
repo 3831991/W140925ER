0bb61e1f65969d1fd337e59a978d3ca29b5b46a5
Your Name <you@example.com>
2026-01-04 18:47:31 +0200

    added

--- JavaScript/lesson14/script.js
+++ JavaScript/lesson14/script.js
@@ -1,4 +1,16 @@
 // פונקציה שמקבלת 3 מספרים ומחזירה סיכום שלהם
-function sum() {
-    
+function sum(n1, n2, n3) {
+    return n1 + n2 + n3;
 }
+
+// פונקציה המקבלת מספר ומחזירה את המספר כולל מע"מ
+function calcVat(num) {
+    return num * 1.18;
+}
+
+// פונקציה המקבלת שם פרטי ומשפחה, מחברת ביניהם ומחזירה
+function sum(fisrtName, lastName) {
+    return `${fisrtName} ${lastName}`;
+}
+
+// פונקציה המקבלת 4 מספרים ומחזירה את הממוצע שלהם
