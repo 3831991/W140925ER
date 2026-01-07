1e68d2ee1f95ebe34d48007608b700a81061cd2b
Your Name <you@example.com>
2026-01-07 20:05:25 +0200

    added

--- JavaScript/lesson15/for/script.js
+++ JavaScript/lesson15/for/script.js
@@ -1,5 +1,11 @@
+const grades = [93, 43, 57, 43, 98, 98, 78, 59, 49, 90, 55, 49, 44, 62, 80, 56, 91, 90, 99, 65];
+
 function sum() {
-    
+    let res = 0;
+
+    for (let i = 0; i < grades.length; i++) {
+
+    }
 }
 
 function avg() {
