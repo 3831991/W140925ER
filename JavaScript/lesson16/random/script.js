42315c153cb63208647b03fd67351e9dc3e073ca
Your Name <you@example.com>
2026-01-11 20:29:40 +0200

    added

--- JavaScript/lesson16/random/script.js
+++ JavaScript/lesson16/random/script.js
@@ -14,3 +14,28 @@ function task2() {
     const num = +document.querySelector(".num1").value;
     document.querySelector(".output2").innerHTML = random(1, num);
 }
+
+function task3() {
+    const length = +document.querySelector(".length").value;
+    const max = +document.querySelector(".max").value;
+    const ul = document.querySelector(".output3");
+    ul.innerHTML = '';
+    
+    for (let i = 0; i < length; i++) {
+        const li = document.createElement("li");
+        li.innerText = random(1, max);
+        ul.appendChild(li);
+    }
+}
+
+// function task3() {
+//     const length = +document.querySelector(".length").value;
+//     const max = +document.querySelector(".max").value;
+//     const arr = [];
+
+//     for (let i = 0; i < length; i++) {
+//         arr.push(`<li>${random(1, max)}</li>`);
+//     }
+
+//     document.querySelector(".output3").innerHTML = arr.join("");
+// }
