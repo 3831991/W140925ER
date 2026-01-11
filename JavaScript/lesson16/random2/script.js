8bf25ab6b6b71dea1e44e33ada1048b176324246
Your Name <you@example.com>
2026-01-11 21:30:08 +0200

    added

--- JavaScript/lesson16/random2/script.js
+++ JavaScript/lesson16/random2/script.js
@@ -1,4 +1,4 @@
-const students = [
+let students = [
     "שרוני ישי",
     "אבו פרחה עובידה",
     "אור אלון",
@@ -26,3 +26,48 @@ const students = [
     "שולי אחלאם",
     "שלמה אינגבר דניאל",
 ];
+
+function showStudents() {
+    const ul = document.querySelector('.frame>ul');
+    ul.innerHTML = "";
+
+    for (const s of students) {
+        const li = document.createElement('li');
+        li.innerText = s;
+        ul.appendChild(li);
+    }
+}
+
+showStudents();
+
+function random() {
+    const arr = [];
+    const len = students.length;
+
+    for (let i = 0; i < len; i++) {
+        const rand = Math.floor(Math.random() * students.length);
+
+        arr.push(students[rand]);
+        students.splice(rand, 1);
+    }
+
+    students = arr;
+    showStudents();
+}
+
+function raffle() {
+    const rand = Math.floor(Math.random() * students.length);
+
+    alert(students[rand]);
+}
+
+function sortASC() {
+    students.sort();
+    showStudents();
+}
+
+function sortDESC() {
+    students.sort();
+    students.reverse();
+    showStudents();
+}
