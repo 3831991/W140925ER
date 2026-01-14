4bcf1927061324b030dfd55706648f3912b33ab2
Your Name <you@example.com>
2026-01-14 20:24:45 +0200

    added

---
+++ JavaScript/lesson17/search/script.js
@@ -0,0 +1,50 @@
+const search = document.querySelector("input[type=search]");
+
+let students = [
+    "שרוני ישי",
+    "אבו פרחה עובידה",
+    "אור אלון",
+    "אלוני ניצן",
+    "ביסמוט אהרון",
+    "בן-אמו רוני",
+    "בנסיאנו איילה",
+    "גולן רוני",
+    "גורביץ אלכסנדר",
+    "גרבר פבל",
+    "דומב יארוסלב",
+    "דנדיס יחיא",
+    "זייגמן בוריס",
+    "חיימוב ארז",
+    "כהן שמעון",
+    "מטר פרנסיס",
+    "סינקביץ` ולידיסלב",
+    "פנאדקה מוחמד",
+    "ציפרפל מקסים",
+    "קאסם לין",
+    "רבינוביץ׳ מקסים",
+    "רג`ואן ליאור",
+    "רוזנבלום איתמר",
+    "רזניק מקסים",
+    "שולי אחלאם",
+    "שלמה אינגבר דניאל",
+];
+
+function showStudents(word) {
+    const ul = document.querySelector("ul");
+    ul.innerHTML = "";
+
+    for (const s of students) {
+        if (!word || s.includes(word)) {
+            const li = document.createElement("li");
+            li.innerHTML = s;
+            ul.appendChild(li);
+        }
+    }
+}
+
+showStudents();
+
+search.addEventListener("input", function(ev) {
+    const word = ev.target.value;
+    showStudents(word);
+});
