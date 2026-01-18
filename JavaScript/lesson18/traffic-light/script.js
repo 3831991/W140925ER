c66128b7bb10734f020625230007f44298a93793
Your Name <you@example.com>
2026-01-18 19:03:27 +0200

    added

--- JavaScript/lesson18/traffic-light/script.js
+++ JavaScript/lesson18/traffic-light/script.js
@@ -3,17 +3,30 @@ const yellowElem = document.querySelector(".yellow");
 const greenElem = document.querySelector(".green");
 let isIntervalActive = false;
 let intervalId;
+let timeoutId;
 
-function trafficError() {
+function trafficError(btn) {
     const elem = document.querySelector('.yellow2');
 
     if (isIntervalActive) {
         clearInterval(intervalId);
         elem.classList.remove("active");
+
+        if (btn) {
+            btn.innerText = "הפעל";
+            btn.style.backgroundColor = "green";
+            btn.style.color = "white";
+        }
     } else {
         intervalId = setInterval(function() {
             elem.classList.toggle("active");
         }, 500);
+        
+        if (btn) {
+            btn.innerText = "עצור";
+            btn.style.backgroundColor = "red";
+            btn.style.color = "white";
+        }
     }
     
     isIntervalActive = !isIntervalActive;
@@ -28,26 +41,30 @@ function lightOff() {
 function red() {
     lightOff();
     redElem.classList.add('active');
-    setTimeout(redAndYellow, 1000 * 5);
+    timeoutId = setTimeout(redAndYellow, 1000 * 5);
 }
 
 function redAndYellow() {
     lightOff();
     redElem.classList.add('active');
     yellowElem.classList.add('active');
-    setTimeout(green, 1000 * 1.5);
+    timeoutId = setTimeout(green, 1000 * 1.5);
 }
 
 function green() {
     lightOff();
     greenElem.classList.add('active');
-    setTimeout(yellow, 1000 * 4);
+    timeoutId = setTimeout(yellow, 1000 * 4);
 }
 
 function yellow() {
     lightOff();
     yellowElem.classList.add('active');
-    setTimeout(red, 1000);
+    timeoutId = setTimeout(red, 1000);
+}
+
+function stopTraffic() {
+    clearTimeout(timeoutId);
 }
 
 trafficError();
