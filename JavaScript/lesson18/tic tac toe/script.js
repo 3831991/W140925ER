8da7b119d35a07f600fb6300d3e28bb68aa1efcb
Your Name <you@example.com>
2026-01-18 20:17:31 +0200

    added

--- JavaScript/lesson18/tic tac toe/script.js
+++ JavaScript/lesson18/tic tac toe/script.js
@@ -1,10 +1,21 @@
 const board = document.querySelector(".board");
+let isX = true;
 
 for (let i = 0; i < 9; i++) {
     const div = document.createElement("div");
 
     div.addEventListener("click", function(ev) {
-        console.log(ev);
+        if (ev.target.innerText) {
+            return;
+        }
+
+        if (isX) {
+            ev.target.innerText = "X";
+        } else {
+            ev.target.innerText = "O";
+        }
+
+        isX = !isX;
     });
 
     board.appendChild(div);
