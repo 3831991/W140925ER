bb09f64378cf11ac589cf147fc0e3fcdc59d2123
Your Name <you@example.com>
2026-01-18 21:15:00 +0200

    added

--- JavaScript/lesson18/tic tac toe/script.js
+++ JavaScript/lesson18/tic tac toe/script.js
@@ -1,11 +1,23 @@
 const board = document.querySelector(".board");
 let isX = true;
+let gameOver = false;
+
+const options = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 
 for (let i = 0; i < 9; i++) {
     const div = document.createElement("div");
 
     div.addEventListener("click", function(ev) {
-        if (ev.target.innerText) {
+        if (gameOver || ev.target.innerText) {
             return;
         }
 
@@ -16,7 +28,28 @@ for (let i = 0; i < 9; i++) {
         }
 
         isX = !isX;
+        check();
     });
 
     board.appendChild(div);
+}
+
+function winner(player, arr) {
+    const divs = board.querySelectorAll("div");
+    arr.forEach(n => divs[n].classList.add('bg'));
+    gameOver = true;
+}
+
+function check() {
+    const divs = board.querySelectorAll("div");
+
+    for (const arr of options) {
+        if (arr.every(n => divs[n].innerText == 'X')) {
+            winner("X", arr);
+            break;
+        } else if (arr.every(n => divs[n].innerText == 'O')) {
+            winner("O", arr);
+            break;
+        }
+    }
 }
