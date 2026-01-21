111d829cb3b735a50a861d7b1b28779deb12f187
Your Name <you@example.com>
2026-01-21 20:38:55 +0200

    added

--- JavaScript/lesson19/array-functions/script.js
+++ JavaScript/lesson19/array-functions/script.js
@@ -1,3 +1,7 @@
 const studentNames = students.slice(0, 20).map(x => `${x.firstName} ${x.lastName}`).join(', ');
 
 document.querySelector(".task1 p").innerHTML = studentNames;
+
+const studentNamesLast20 = students.slice(-20).map((x, i) => `${i + 1}. ${x.firstName} ${x.lastName}`).join('<br>');
+
+document.querySelector(".task2 p").innerHTML = studentNamesLast20;
