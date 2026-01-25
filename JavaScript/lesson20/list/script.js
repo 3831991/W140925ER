107a536720d0a92e29f5dcfdad96c2596fd5526b
Your Name <you@example.com>
2026-01-25 21:08:12 +0200

    added

--- JavaScript/lesson20/list/script.js
+++ JavaScript/lesson20/list/script.js
@@ -15,24 +15,29 @@ function createTask(obj, i) {
     const div = document.createElement("div");
     const remove = document.createElement("span");
 
+    // אם המשימה בוצעה, מסמנים אותה בהתאם
     if (obj.checked) {
         li.classList.add("checked");
         input.checked = true;
     } else {
+        // מאפשר לערוך
         div.contentEditable = true;
     }
 
     input.type = "checkbox";
+    // כל שינוי בסטטוס המשימה
     input.addEventListener("change", ev => {
         obj.checked = ev.target.checked;
         li.classList.toggle("checked");
     });
 
     div.innerHTML = obj.subject;
+    // מעקב אחר שינויים בתוכן המשימה
     div.addEventListener("input", ev => {
         obj.subject = ev.target.innerText;
     });
 
+    // לחצן מחיקה
     remove.innerText = 'X';
     remove.className = "remove";
     remove.addEventListener("click", () => {
@@ -57,4 +62,5 @@ function newTask() {
     createTask(obj, tasks.length - 1);
 }
 
+// מפעיל את הפונקציה של הוספת משימה על כל אובייקט במערך
 tasks.forEach(createTask);
