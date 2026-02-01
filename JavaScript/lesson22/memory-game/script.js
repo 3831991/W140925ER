65432f4cae6561c6e7f45b8387a176ae8324579e
Your Name <you@example.com>
2026-02-01 18:06:01 +0200

    added

---
+++ JavaScript/lesson22/memory-game/script.js
@@ -0,0 +1,20 @@
+const fruits = [
+    { id: 1, name: 'תפוח', icon: '🍎' },
+    { id: 2, name: 'בננה', icon: '🍌' },
+    { id: 3, name: 'תפוז', icon: '🍊' },
+    { id: 4, name: 'אבטיח', icon: '🍉' },
+    { id: 5, name: 'ענבים', icon: '🍇' },
+    { id: 7, name: 'רימון', icon: '🍎' },
+    { id: 8, name: 'תמר', icon: '🌴' },
+    { id: 9, name: 'זית', icon: '🫒' },
+    { id: 10, name: 'אגס', icon: '🍐' },
+    { id: 11, name: 'אפרסק', icon: '🍑' },
+    { id: 12, name: 'אננס', icon: '🍍' },
+    { id: 13, name: 'מנגו', icon: '🥭' },
+    { id: 14, name: 'דובדבן', icon: '🍒' },
+    { id: 15, name: 'תות', icon: '🍓' },
+    { id: 16, name: 'לימון', icon: '🍋' },
+    { id: 17, name: 'קיווי', icon: '🥝' },
+    { id: 18, name: 'מלון', icon: '🍈' },
+];
+
