c61bb43da5b3da875a5e161b7d5c708d63f1ed96
Your Name <you@example.com>
2026-02-04 20:10:43 +0200

    added

---
+++ JavaScript/lesson23/script.js
@@ -0,0 +1,27 @@
+const frame = document.querySelector(".frame");
+
+const setTickets = tickets => {
+    tickets.forEach(ticket => {
+        const div = document.createElement("div");
+        div.className = "ticket";
+
+        div.innerHTML = `
+            <header>
+                <h2>${ticket.fullName}</h2>
+                <span class="date">${moment(ticket.createTime).format("DD/MM/YY")}</span>
+            </header>
+
+            <p>${ticket.message}</p>
+
+            <footer>
+                ${ticket.phone} | ${ticket.email}
+            </footer>
+        `;
+
+        frame.appendChild(div);
+    });
+}
+
+fetch("https://api.shipap.co.il/contact")
+.then(res => res.json())
+.then(setTickets);
