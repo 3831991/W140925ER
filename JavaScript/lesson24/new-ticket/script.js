350901cd45ae101b2ea7a2771c023bd296dbeae7
Your Name <you@example.com>
2026-02-11 20:12:12 +0200

    added

--- JavaScript/lesson24/new-ticket/script.js
+++ JavaScript/lesson24/new-ticket/script.js
@@ -1,5 +1,21 @@
-function addTicket(ev) {
+async function addTicket(ev) {
     ev.preventDefault();
 
-    
+    const { email, phone, name, message } = ev.target;
+
+    const obj = {
+        email: email.value,
+        phone: phone.value,
+        fullName: name.value,
+        message: message.value,
+    };
+
+    const res = await fetch(`https://api.shipap.co.il/contact`, {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(obj),
+    });
+
+    const ticket = await res.json();
+    console.log(ticket);
 }
