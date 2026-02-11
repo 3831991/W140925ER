86855a5d1ae2684845fc615880424fcc2682fa54
Your Name <you@example.com>
2026-02-11 21:01:14 +0200

    added

---
+++ JavaScript/lesson24/snake/script.js
@@ -0,0 +1,25 @@
+const height = 50;
+const width = 40;
+const length = 10;
+
+const snake = new Array(length).fill().map((x, i) => i);
+snake.reverse();
+
+const divs = [];
+const board = document.querySelector(".board");
+board.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+
+// Create board
+for (let i = 0; i < width * height; i++) {
+    const div = document.createElement('div');
+    board.appendChild(div);
+    divs.push(div);
+}
+
+function drawSnake() {
+    snake.forEach(num => {
+        divs[num].classList.add('snake');
+    });
+}
+
+drawSnake();
